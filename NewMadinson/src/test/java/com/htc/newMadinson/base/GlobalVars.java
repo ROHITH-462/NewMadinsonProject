32a9d2a0e497b0a71fb3a5ff588d56d3df7e3218
Rohith <dinesh.rohi@gmail.com>
2021-03-17 10:07:36 +0530

    Excel added into project location

--- NewMadinson/src/test/java/com/htc/newMadinson/base/GlobalVars.java
+++ NewMadinson/src/test/java/com/htc/newMadinson/base/GlobalVars.java
@@ -3,7 +3,7 @@ package com.htc.newMadinson.base;
 public class GlobalVars {
 	
 	public static int explicitWaitTime = 10;
-	public static String passFilePath = "C:\\Users\\DELL\\git\\NewMadinsonProject\\NewMadinson\\ScreenShots\\Pass";
-	public static String failFilePath = "C:\\Users\\DELL\\git\\NewMadinsonProject\\NewMadinson\\ScreenShots\\Fail";
+	public static String passFilePath = "C:\\Users\\DELL\\git\\NewMadinsonProject\\NewMadinson\\ScreenShots\\Pass\\";
+	public static String failFilePath = "C:\\Users\\DELL\\git\\NewMadinsonProject\\NewMadinson\\ScreenShots\\Fail\\";
 	
 }
